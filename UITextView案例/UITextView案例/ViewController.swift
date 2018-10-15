fd709b202082f53186963f6f1e2802e8a46eaae7
杨帆 <yangfanvw@qq.com>
2018-10-15 17:22:39 +0800

    新增UITextView案例

---
+++ UITextView案例/UITextView案例/ViewController.swift
@@ -0,0 +1,49 @@
+//
+//  ViewController.swift
+//  UITextView案例
+//
+//  Created by 杨帆 on 2018/10/15.
+//  Copyright © 2018 ABC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITextFieldDelegate {
+
+    @IBAction func endEdit(_ sender: Any) {
+        
+        print("输入结束")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        self.view.endEditing(true)
+    }
+    
+    //UITextFieldDelegate代理方法
+    func textFieldDidEndEditing(_ textField: UITextField, reason: UITextField.DidEndEditingReason) {
+        
+        if let text = textField.text {
+            
+            print(text)
+        }
+        
+    }
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        
+        print(string)
+        
+        return true
+        
+    }
+
+
+}
+
