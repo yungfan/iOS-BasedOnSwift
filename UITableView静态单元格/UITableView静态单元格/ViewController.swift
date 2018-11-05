b15988667c1720e8e557400b821e03406d138907
杨帆 <yangfanvw@qq.com>
2018-11-05 21:36:42 +0800

    增加UITableView静态单元格案例

---
+++ UITableView静态单元格/UITableView静态单元格/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UITableView静态单元格
+//
+//  Created by 杨帆 on 2018/11/5.
+//  Copyright © 2018 ABC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
