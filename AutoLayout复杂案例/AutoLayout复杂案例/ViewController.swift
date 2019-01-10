b2c2e4c097875788fc578e5266e6fd5796d77daf
杨帆 <yangfanvw@qq.com>
2019-01-10 21:35:35 +0800

    新增AutoLayout

---
+++ AutoLayout复杂案例/AutoLayout复杂案例/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AutoLayout复杂案例
+//
+//  Created by stu1 on 2019/1/10.
+//  Copyright © 2019年 abc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ AutoLayout简单案例/AutoLayout简单案例/ViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ViewController.swift
+//  AutoLayout简单案例
+//
+//  Created by stu1 on 2019/1/10.
+//  Copyright © 2019年 abc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var redView: UIView!
+    
+    @IBOutlet weak var bottom: NSLayoutConstraint!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        
+        //设置frame有效果
+        //self.redView.frame = CGRect(x: 0, y: 0, width: 200, height: 200)
+        
+        
+        UIView.animate(withDuration: 2.0) {
+            
+             //修改frame可以产生动画
+             //self.redView.frame = CGRect(x: 0, y: 0, width: 200, height: 200)
+            
+            //直接修改约束 不产生动画
+            self.bottom.constant = 200.0
+            //必须调用下面的方法才可以
+            self.view.layoutIfNeeded()
+        }
+    }
+
+}
+
