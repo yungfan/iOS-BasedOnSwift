4fffe69741ab82d6a00384d8b893f48a63a61629
杨帆 <yangfanvw@qq.com>
2019-03-04 21:36:08 +0800

    新增国际化案例

---
+++ 其他/I18NDemo/I18NDemo/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  I18NDemo
+//
+//  Created by 杨帆 on 2019/3/4.
+//  Copyright © 2019 杨帆. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        let alert = UIAlertController(title: NSLocalizedString("title", comment: "") , message: NSLocalizedString("message", comment: ""), preferredStyle: .alert)
+        
+        
+        let action = UIAlertAction(title:NSLocalizedString("btnTitle", comment: ""), style: .cancel, handler: nil)
+        
+        alert.addAction(action)
+        
+        self.present(alert, animated: true, completion: nil)
+    }
+
+}
+
