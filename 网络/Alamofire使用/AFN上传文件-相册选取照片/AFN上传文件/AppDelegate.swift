4936c77878d684e574a0340decea3a7a2b4a7d35
杨帆 <yangfanvw@qq.com>
2021-01-06 16:57:22 +0800

    增加授权

--- 网络/Alamofire使用/AFN上传文件-相册选取照片/AFN上传文件/AppDelegate.swift
+++ 网络/Alamofire使用/AFN上传文件-相册选取照片/AFN上传文件/AppDelegate.swift
@@ -5,15 +5,14 @@
 //  Created by 杨帆 on 2020/12/30.
 //
 
+import Photos
 import UIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        requestAuthorization()
         return true
     }
 
@@ -30,7 +29,21 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
-
-
 }
 
+extension AppDelegate {
+    func requestAuthorization() {
+        PHPhotoLibrary.requestAuthorization(for: .readWrite) { status in
+            switch status {
+            case .limited:
+                print("限制")
+            case .denied:
+                print("拒绝")
+            case .authorized:
+                print("授权")
+            default:
+                print("默认")
+            }
+        }
+    }
+}
